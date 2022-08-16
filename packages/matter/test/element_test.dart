67bcbe9a408ad0594e0ac748da09b2637e608b7f
Thipok Cholsaipant <thipok.ch@gmail.com>
2022-08-16 17:26:57 +0700

    build: fix packages name

--- packages/matter/test/element_test.dart
+++ packages/matter/test/element_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:element/element.dart';
+import 'package:matter/element.dart';
 
 void main() {
   test('adds one to input values', () {
