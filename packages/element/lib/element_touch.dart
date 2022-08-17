05c290c1b945627eaf1abfc21557b7307736506c
Thipok Cholsaipant <thipok.ch@gmail.com>
2022-08-17 18:45:40 +0700

    feat: gaimon for haptics

---
+++ packages/element/lib/element_touch.dart
@@ -0,0 +1,15 @@
+import 'package:gaimon/gaimon.dart';
+
+class ElementTouch {
+  ElementTouch._();
+
+  static const select = Gaimon.selection;
+  static const error = Gaimon.error;
+  static const success = Gaimon.success;
+  static const warn = Gaimon.warning;
+  static const heavy = Gaimon.heavy;
+  static const medium = Gaimon.medium;
+  static const light = Gaimon.light;
+  static const rigid = Gaimon.rigid;
+  static const soft = Gaimon.soft;
+}
